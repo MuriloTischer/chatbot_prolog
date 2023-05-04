a01ffd2eccb20975101ad50bacf93f949858bf09
Murilo Tadeu D. Tischer <78828940+MuriloTischer@users.noreply.github.com>
2023-05-04 19:35:04 -0300

    Create second_db.pl

---
+++ second_db.pl
@@ -0,0 +1,38 @@
+% Definindo a base de conhecimentos II
+% Respostas para perguntas sobre redes de computadores
+% Base de dados II - Perguntas mais avançadas sobre redes de computadores
+
+consultar('O_que_eh_Ethernet','Ethernet eh uma tecnologia de redes de computadores que define um conjunto de regras para a comunicacao de dados em uma rede local (LAN), utilizando cabos de cobre ou fibras opticas. A Ethernet eh a tecnologia de rede mais comum atualmente e suporta velocidades de transmissao de dados variando de 10 Mbps a 100 Gbps.').
+
+consultar('O_que_eh_Enderecamento_na_ethernet_e_quais_sao_os_seus_servicos','O enderecamento na Ethernet eh a forma de identificar cada dispositivo conectado a uma rede local (LAN) que utiliza essa tecnologia. Na Ethernet, cada dispositivo eh identificado por um endereco fisico unico, conhecido como endereco MAC (Media Access Control). Os principais servicos oferecidos pela Ethernet incluem a comunicacao de dados em alta velocidade, a deteccao de colisoes na rede e o controle de acesso ao meio fisico.').
+
+consultar('O_que_eh_um_Protocolo_MAC','O Protocolo MAC (Media Access Control) eh o conjunto de regras que define como os dispositivos em uma rede local (LAN) devem se comunicar entre si utilizando a Ethernet. O Protocolo MAC utiliza enderecos fisicos unicos, conhecidos como enderecos MAC, para identificar cada dispositivo conectado eh rede.').
+
+consultar('O_que_eh_Padrao_Gigabit_Ethernet','Gigabit Ethernet eh um padrao de redes de computadores que permite a transmissao de dados em velocidades de ate 1 gigabit por segundo (Gbps). O Padrao Gigabit Ethernet eh a versao da tecnologia Ethernet que suporta essa velocidade de transmissao de dados. Ele utiliza cabos de cobre ou fibras opticas para a transmissao de dados em redes locais (LANs) de alta velocidade.').
+
+consultar('O_que_eh_Metro_Ethernet','Metro Ethernet eh uma tecnologia de redes de computadores que permite a transmissao de dados em alta velocidade em redes metropolitanas, que sao redes que abrangem uma area geografica maior do que uma rede local (LAN), mas menor do que uma rede de longa distoncia (WAN). O Metro Ethernet utiliza a tecnologia Ethernet para a transmissao de dados, e pode suportar velocidades de transmissao de dados de ate 10 Gbps.').
+
+consultar('O_que_eh_o_protocolo_TCP/IP','O protocolo TCP/IP eh o conjunto de regras que define como os dados devem ser transmitidos entre dispositivos em redes de computadores, especialmente na internet. O protocolo TCP/IP eh composto por dois protocolos principais: o protocolo TCP (Transmission Control Protocol) e o protocolo IP (Internet Protocol). O protocolo TCP eh responsavel pelo controle de transmissao dos dados, enquanto o protocolo IP eh responsavel pelo roteamento dos dados entre os dispositivos.').
+
+consultar('Quais_sao_os_protocolos_que_constituem_o_TCP','O protocolo TCP eh composto por varios protocolos secundarios, que incluem o protocolo de controle de congestionamento (TCP congestion control). O TCP eh composto por dois protocolos principais: o protocolo de controle de transmissao (TCP) e o protocolo de internet (IP).').
+
+consultar('O_que_eh_IGRP','O Interior Gateway Routing Protocol (IGRP) eh um protocolo de roteamento que foi desenvolvido pela Cisco Systems para uso em redes de grande porte e complexas. Ele eh um protocolo de roteamento vetor de distancia que eh usado para determinar as rotas mais eficientes para as redes de destino.').
+
+consultar('O_que_eh_EIGRP','O Enhanced Interior Gateway Routing Protocol (EIGRP) eh um protocolo de roteamento avancado que foi desenvolvido pela Cisco Systems. Ele eh um protocolo hibrido que combina as melhores caracteristicas dos protocolos de vetor de distancia e de estado de link para fornecer um roteamento rapido e eficiente.').
+
+consultar('O_que_eh_SMTP','SMTP significa Simple Mail Transfer Protocol. Eh o protocolo padrao usado para enviar e receber e-mails na Internet. Ele eh um protocolo de aplicacao que eh responsavel por enviar e-mails de um servidor de e-mail para outro.').
+
+consultar('O_que_eh_Telnet','O Telnet eh um protocolo de rede que permite que um usuario se conecte a um servidor remoto e controle o servidor como se estivesse sentado diretamente na frente dele. Ele eh usado principalmente para acesso a hosts remotos por meio de uma rede de computadores.').
+
+consultar('O_que_eh_FTP','FTP significa File Transfer Protocol. Eh um protocolo de transferencia de arquivos que eh usado para transferir arquivos entre computadores em uma rede. Ele permite que os usuarios copiem arquivos de um computador para outro e vice-versa.').
+
+consultar('O_que_eh_http','HTTP significa Hypertext Transfer Protocol. Eh um protocolo de aplicacao que eh usado para transferir dados pela World Wide Web. Ele permite que as informacoes sejam trocadas entre um cliente e um servidor, permitindo que o cliente acesse e visualize paginas da web.').
+
+consultar('O_que_eh_camada_de_rede','A camada de rede eh a terceira camada do modelo OSI. Ela eh responsavel por fornecer servicos de roteamento e encaminhamento de dados entre redes diferentes. Ela eh responsavel por determinar a melhor rota para enviar os pacotes de dados de origem para o destino.').
+
+consultar('O_que_eh_IPv4','O IPv4 eh a quarta versao do Protocolo de Internet. Ele eh um protocolo de camada de rede que eh usado para fornecer conectividade de rede para computadores e outros dispositivos. O IPv4 usa enderecos IP de 32 bits e eh capaz de suportar ate cerca de 4,3 bilhoes de enderecos unicos.').
+
+consultar('O_que_eh_ICMP','ICMP significa Internet Control Message Protocol. Eh um protocolo de camada de rede que eh usado para fornecer mensagens de erro e de controle de rede. Ele eh usado principalmente para fornecer feedback aos hosts de origem sobre problemas de entrega de pacotes.').
+
+consultar('fim',
+	'Ate a proxima! Foi um prazer poder te ajudar!').

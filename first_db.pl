9551061bb0c5045bdcdc73b2101e7caa0582d0cd
Murilo Tadeu D. Tischer <78828940+MuriloTischer@users.noreply.github.com>
2023-05-04 19:33:09 -0300

    Create first_db.pl

---
+++ first_db.pl
@@ -0,0 +1,50 @@
+% Definindo o banco de conhecimentos I
+% Respostas para perguntas sobre redes de computadores
+% Base de dados I - Topologias de redes
+
+consultar('O_que_eh_uma_rede_de_computadores','Uma rede de computadores eh um conjunto de dispositivos interconectados que sao capazes de se comunicar entre si para compartilhar recursos e trocar informacoes.').
+
+consultar('Qual_a_classificacao_de_redes','As redes de computadores podem ser classificadas em LAN, MAN e WAN, dependendo de sua abrangencia geografica.').
+
+consultar('O_que_eh_um_roteador','Um roteador eh um dispositivo de rede que eh usado para encaminhar pacotes de dados entre redes diferentes.').
+
+consultar('O_que_eh_uma_placa_de_rede','Uma placa de rede eh um componente de hardware que eh usado para conectar um computador a uma rede de computadores.').
+
+consultar('O_que_eh_um_Modem','Um modem eh um dispositivo que converte os sinais digitais de um computador em sinais analogicos para transmissao por uma linha telefonica e converte os sinais analogicos recebidos de volta em sinais digitais para o computador.').
+
+consultar('O_que_eh_um_repetidor','Um repetidor eh um dispositivo que amplifica e regenera os sinais de rede para que eles possam viajar mais longe atraves de um meio fisico.').
+
+consultar('O_que_eh_um_Switch','Um switch eh um dispositivo de rede que eh usado para conectar dispositivos em uma rede local e encaminhar pacotes de dados entre eles.').
+
+consultar('O_que_eh_uma_LAN','Uma LAN (Local Area Network) eh uma rede de computadores que abrange uma area geografica relativamente pequena como uma casa um escritorio ou um edificio.').
+
+consultar('O_que_eh_uma_MAN','Uma MAN (Metropolitan Area Network) eh uma rede de computadores que abrange uma area geografica maior do que uma LAN geralmente uma cidade ou uma area metropolitana.').
+
+consultar('O_que_eh_uma_WAN','Uma WAN (Wide Area Network) eh uma rede de computadores que abrange uma area geografica muito grande, como um pais ou ate mesmo o mundo inteiro.').
+
+consultar('O_que_eh_internet','A internet eh uma rede mundial de computadores que conecta milhoes de dispositivos e permite a comunicacao e o compartilhamento de informacoes em todo o mundo.').
+
+consultar('O_que_eh_Correio_eletronico','O correio eletronico, ou e-mail, eh um servico de comunicacao eletronica que permite enviar e receber mensagens de texto e anexos pela internet.').
+
+consultar('O_que_eh_um_Cabo_coaxial','Um cabo coaxial eh um tipo de cabo utilizado para transmitir sinais de alta frequencia, como sinais de televisao, internet e telefone. Ele eh composto por um fio central cercado por um condutor cilindrico, que eh revestido por um isolante e uma malha metalica. O cabo coaxial eh amplamente utilizado em redes de computadores para conectar dispositivos e transmissao de sinais de dados.').
+
+consultar('O_que_eh_um_Cabo_de_par_trancado','Um cabo de par trancado eh um tipo de cabo composto por dois fios de cobre entrelacados que sao torcidos juntos em uma serie de pares. Ele eh frequentemente utilizado para conectar dispositivos de rede como computadores, switches e roteadores. O cabo de par trancado eh capaz de transmitir dados em alta velocidade e possui uma boa resistencia a interferencias eletromagneticas.').
+
+consultar('O_que_eh_a_Fibra_optica','A fibra optica eh um tipo de cabo que utiliza luz para transmitir informacoes. Ela eh composta por um nucleo de vidro ou plastico cercado por uma capa protetora, e eh capaz de transmitir dados em altissimas velocidades e com uma grande largura de banda. A fibra oiptica eh amplamente utilizada em redes de computadores de alta velocidade, como as redes de area metropolitana (MAN) e as redes de longa distância (WAN).').
+
+consultar('O_que_eh_Radiofrequencia','Radiofrequencia eh uma faixa de frequencia do espectro eletromagnetico que eh utilizada para transmitir informacoes sem fio. Essa faixa de frequencia abrange desde alguns kilohertz ate alguns gigahertz. A radiofrequencia eh utilizada em diversas tecnologias de comunicacao sem fio, como o radio, a televisao, os telefones celulares e as redes de internet sem fio.').
+
+consultar('O_que_sao_Topologias_de_redes','Topologia de rede eh o arranjo fisico dos dispositivos em uma rede de computadores. Existem diversos tipos de topologias, como a topologia em barramento, a topologia em estrela, a topologia em anel e a topologia em malha. Cada tipo de topologia possui suas proprias caracteristicas e vantagens, e eh escolhido de acordo com as necessidades da rede.').
+
+consultar('O_que_eh_uma_Topologia_Parcialmente_Ligada','Uma topologia parcialmente ligada eh um tipo de topologia de rede em que alguns dispositivos estao conectados diretamente uns aos outros, enquanto outros estao conectados indiretamente atraves de um ou mais dispositivos intermedierios. Essa topologia eh uma combinaco da topologia em estrela e da topologia em malha.').
+
+consultar('O_que_eh_uma_Topologia_em_Barramento','A topologia em barramento eh um tipo de topologia de rede em que todos os dispositivos estao conectados a um unico cabo, chamado de barramento. A comunicacao entre os dispositivos eh feita enviando pacotes de dados atraves do barramento. Essa topologia eh utilizada principalmente em redes locais (LANs).').
+
+consultar('O_que_eh_uma_Topologia_Ponto_a_Ponto','Uma Topologia Ponto a Ponto eh uma configuracao de rede na qual dois dispositivos sao diretamente conectados entre si, sem a necessidade de um dispositivo intermediario, como um switch ou um roteador.').
+
+consultar('O_que_eh_uma_Topologia_Multiponto','Uma Topologia Multiponto eh uma configuracao de rede na qual varios dispositivos sao conectados em uma unica rede, permitindo a comunicacao entre todos eles.').
+
+consultar('O_que_eh_uma_Topologia_em_Estrela','Uma Topologia em Estrela eh uma configuracao de rede na qual todos os dispositivos sao conectados a um unico dispositivo central, como um switch ou um hub. A comunicacao entre os dispositivos da rede eh realizada atraves do dispositivo central.').
+
+consultar('fim',
+	'Ate a proxima! Foi um prazer poder te ajudar!').

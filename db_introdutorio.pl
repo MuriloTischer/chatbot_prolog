9fff4b4026697501be992b56dadfa8fac5ca22ec
Murilo Tadeu D. Tischer <78828940+MuriloTischer@users.noreply.github.com>
2023-05-12 13:50:59 -0300

    Create db_introdutorio.pl

---
+++ db_introdutorio.pl
@@ -0,0 +1,23 @@
+% Definindo o banco de conhecimentos I
+% Autor: Murilo Tadeu Domingos Tischer.
+% Projeto: ChatBot de Perguntas e Respostas.
+% Respostas para perguntas sobre redes de computadores
+% Base de dados para Introdução
+
+consultar('Ola','Ola, como voce esta?..Como posso te ajudar?').
+
+consultar('Ola, tudo bem?','Eu estou bem, obrigado por perguntar e voce como voce esta?').
+
+consultar('estou bem tambem','Que bom, Em que eu posso te ajudar?').
+
+consultar('estou bem','Que bom, Em que eu posso te ajudar?').
+
+consultar('Qual sua idade?','Como eu sou apenas uma Inteligencia Artificial, eu nao tenho uma idade propriamente dita, mas eu fui criado a menos de um mes por um humano.').
+
+consultar('Qual humano criou voce?','Eu fui criado pelo humano Murilo Tischer').
+
+consultar('Quem é Murilo Tischer e o que ele faz?','Murilo Tischer eh um Desenvolvedor Web, especialista nas linguagens Java, Dart e Flutter. 
+Tambem conhecedor da linguagem para banco de dados PostgreSQL e recentemente usou a linguagem PROLOG para me desenvolver').
+
+consultar('Quais tipos de perguntas voce cosegue me responder?', 'No momento eu consigo apenas responder perguntas sobre Redes de Computadores').
+
